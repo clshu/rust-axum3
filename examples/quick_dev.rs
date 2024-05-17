08e014aab96c6c935b6e7cc1ef6a607d11d317dc
clshu <clshu2020@gmail.com>
2024-05-16 19:27:32 -0700

    E2-01 - sqlb/sqlx to sea-query/modql

--- examples/quick_dev.rs
+++ examples/quick_dev.rs
@@ -1,7 +1,7 @@
 #![allow(unused)] // For beginning only.
 
 use anyhow::Result;
-use serde_json::json;
+use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -30,34 +30,8 @@ async fn main() -> Result<()> {
 			}
 		}),
 	);
+
 	req_create_task.await?.print().await?;
-
-	let req_update_task = hc.do_post(
-		"/api/rpc",
-		json!({
-			"id": 1,
-			"method": "update_task",
-			"params": {
-				"id": 1000, // Hardcode the task id.
-				"data": {
-					"title": "task BB"
-				}
-			}
-		}),
-	);
-	req_update_task.await?.print().await?;
-
-	let req_delete_task = hc.do_post(
-		"/api/rpc",
-		json!({
-			"id": 1,
-			"method": "delete_task",
-			"params": {
-				"id": 1001 // Harcode the task id
-			}
-		}),
-	);
-	req_delete_task.await?.print().await?;
 
 	let req_list_tasks = hc.do_post(
 		"/api/rpc",
@@ -68,13 +42,58 @@ async fn main() -> Result<()> {
 	);
 	req_list_tasks.await?.print().await?;
 
+	let req_update_task = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "update_task",
+			"params": {
+				"id": 1008, // Hardcode the task id.
+				"data": {
+					"title": "task BB"
+				}
+			}
+		}),
+	);
+	req_update_task.await?.print().await?;
+
+	let req_list_tasks1 = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "list_tasks"
+		}),
+	);
+	req_list_tasks1.await?.print().await?;
+
+	let req_delete_task = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "delete_task",
+			"params": {
+				"id": 1008 // Harcode the task id
+			}
+		}),
+	);
+	req_delete_task.await?.print().await?;
+
+	let req_list_tasks2 = hc.do_post(
+		"/api/rpc",
+		json!({
+			"id": 1,
+			"method": "list_tasks"
+		}),
+	);
+	req_list_tasks2.await?.print().await?;
+
 	let req_logoff = hc.do_post(
 		"/api/logoff",
 		json!({
 			"logoff": true
 		}),
 	);
-	// req_logoff.await?.print().await?;
+	req_logoff.await?.print().await?;
 
 	Ok(())
 }
